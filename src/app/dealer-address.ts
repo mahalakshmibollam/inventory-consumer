3bd98d385e8630d035e4af3c7984e0fcb24d704f
Mahalakshmi <mahalakshmi.bollam@gmail.com>
2022-05-10 13:13:43 +0530

    Inventory consumer 1.0

---
+++ src/app/dealer-address.ts
@@ -0,0 +1,18 @@
+export class DealerAddress {
+
+    id: number=0;
+    email: string='';
+    fname: string | any;
+    lname: string | any;
+    password: string | any;
+    dob: Date | any;
+    phoneNo:string | any;
+ 
+   // addressId: number;
+    street: string | any;
+    city: string | any;
+    pincode:number=0;
+ 
+    active: boolean=false;
+    
+}
